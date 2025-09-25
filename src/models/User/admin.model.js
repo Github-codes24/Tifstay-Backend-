ec7f22820fdb8309a762743935be59179d2d83ab
Mohit Bhirani <mankudeveloper786@gmail.com>
2025-09-25 09:05:54 +0530

    hosteljj

--- src/models/User/admin.model.js
+++
@@ -1,9 +0,0 @@
-const User = require('./user.model');
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-  roleLevel: { type: String, enum: ['super', 'moderator'], default: 'moderator' },
-  address: String
-});
-
-module.exports = User.discriminator('admin', AdminSchema);
